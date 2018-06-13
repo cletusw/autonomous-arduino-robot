b83cc5770474f8f297473b3a77972c9e0aa43aad
Clayton Watts <cletusw@gmail.com>
2018-06-12 22:51:39 -0700

    works, but too slow to react

--- autonomous_robot.ino
+++ autonomous_robot.ino
@@ -2,9 +2,9 @@
 #include <Adafruit_MotorShield.h>
 #include <PID_v1.h>
 #include "SR04.h"
+#include "MedianFilter.h"
 #include "MotorController.h"
 #include "encoders.h"
-#include "median5.h"
 
 // PID
 const double Kp = 0.05;
@@ -21,15 +21,24 @@ Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
 MotorController leftMotor(*motor1, maxMotorCommand, pidSamplePeriodMillis, Kp, Ki, Kd);
 MotorController rightMotor(*motor2, maxMotorCommand, pidSamplePeriodMillis, Kp, Ki, Kd);
 
+// For all distance readings
+const uint8_t maxDistance = 100;
+const uint8_t minDistance = 10;
+
 // Ultrasonic sensor
-const int TRIG_PIN = 10;
-const int ECHO_PIN = 11;
+const uint8_t TRIG_PIN = 10;
+const uint8_t ECHO_PIN = 11;
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 
-// For smoothing with the median-of-5 filter
-const int numUltrasonicReadings = 5;
-int ultrasonicReadings[numUltrasonicReadings];
-uint8_t ultrasonicReadIndex = 0; // the index of the oldest/next-to-be-overwritten reading
+// IR
+const uint8_t IR0_PIN = A0;
+const uint8_t IR1_PIN = A1;
+
+// For smoothing
+const uint8_t distanceFilterSize = 7;
+MedianFilter ultrasonicFilter(distanceFilterSize, 0);
+MedianFilter ir0(distanceFilterSize, 0);
+MedianFilter ir1(distanceFilterSize, 0);
 
 
 void setup() {
@@ -39,12 +48,15 @@ void setup() {
 
   setUpEncoderPins();
 
+  pinMode(IR0_PIN, INPUT);
+  pinMode(IR1_PIN, INPUT);
+
+  initializeSmoothing();
+
   leftMotor.setDesiredSpeed(0);
   rightMotor.setDesiredSpeed(0);
   leftMotor.drive();
   rightMotor.drive();
-
-  initializeSmoothing();
 
   nextMillis = millis() + pidSamplePeriodMillis;
 }
@@ -61,19 +73,30 @@ void loop() {
     }
   }
 
-  if (readUltrasonicSensor() < 20) {
-    leftMotor.setDesiredSpeed(0);
-    rightMotor.setDesiredSpeed(0);
-  }
-
   if (currentMillis > nextMillis) {
     nextMillis += pidSamplePeriodMillis;
 
     leftMotor.updateMeasuredSpeed(encoderDCountInternal);
     rightMotor.updateMeasuredSpeed(encoderBCountInternal);
 
-    leftMotor.log(Serial);
+//    leftMotor.log(Serial);
 //    rightMotor.log(Serial);
+
+    getDistanceReadings();
+    uint16_t ir0Distance = ir0.out();
+    uint16_t ultrasonicDistance = ultrasonicFilter.out();
+    uint16_t ir1Distance = ir1.out();
+    Serial.print(ir0Distance);
+    Serial.print(' ');
+    Serial.print(ultrasonicDistance);
+    Serial.print(' ');
+    Serial.print(ir1Distance);
+    Serial.println();
+
+    if (ultrasonicDistance < 30 || ir0Distance < 30 || ir1Distance < 30) {
+      leftMotor.setDesiredSpeed(0);
+      rightMotor.setDesiredSpeed(0);
+    }
   }
   
   leftMotor.drive();
@@ -82,16 +105,26 @@ void loop() {
 
 void initializeSmoothing() {
   // fill buffer with readings
-  for (int i = 0; i < numUltrasonicReadings; i++) {
-    ultrasonicReadings[i] = sr04.Distance();
+  for (uint8_t i = 0; i < distanceFilterSize; i++) {
+    getDistanceReadings();
+    delay(25);
   }
 }
 
-int readUltrasonicSensor() {
-  int distance = sr04.Distance(); //  2cm - 400cm (ish)
-  ultrasonicReadings[ultrasonicReadIndex] = distance;
-  ultrasonicReadIndex = (ultrasonicReadIndex + 1) % numUltrasonicReadings;
-  return distance;
+// Need to wait 38ms +/- 10ms between IR readings,
+// so you'll probably need a delay(25); after calling this
+void getDistanceReadings() {
+  ir0.in(irReadingToDistance(analogRead(IR0_PIN)));
+  ir1.in(irReadingToDistance(analogRead(IR1_PIN)));
+  ultrasonicFilter.in(constrain(
+      sr04.Distance(), minDistance, maxDistance)); // includes a delay(25)
+}
+
+// Returns distance in cm
+uint8_t irReadingToDistance(uint16_t value) {
+  if (value < 10) value = 10;
+  return constrain(
+      (6787.0 / (value - 3.0)) - 4.0, minDistance, maxDistance);
 }
 
 
