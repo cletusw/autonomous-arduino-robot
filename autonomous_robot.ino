2b153ffd36221a0f4545657f7c19fd64bbae32f9
Clayton Watts <cletusw@gmail.com>
2018-06-23 23:20:34 -0700

    Update to NewPing so it's more responsive!

--- autonomous_robot.ino
+++ autonomous_robot.ino
@@ -1,10 +1,12 @@
+#include <limits.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include <PID_v1.h>
-#include "SR04.h"
+#include <NewPing.h>
 #include "MedianFilter.h"
 #include "MotorController.h"
 #include "encoders.h"
+
 
 // PID
 const double Kp = 0.05;
@@ -21,15 +23,16 @@ MotorController leftMotor(*motor1, encoderDCountInternal, maxMotorCommand, pidSa
 MotorController rightMotor(*motor2, encoderBCountInternal, maxMotorCommand, pidSamplePeriodMillis, Kp, Ki, Kd);
 
 // For all distance readings
-const uint8_t maxDistance = 100;
+const uint8_t maxDistance = 100; // <=255 so it fits in the uint8_t
 const uint8_t minDistance = 10;
-const unsigned long distanceSamplePeriodMillis = 50;
+const unsigned long distanceSamplePeriodMillis = 75; // need to wait 38ms +/- 10ms between IR readings,
 unsigned long nextMillis = 0;
 
 // Ultrasonic sensor
-const uint8_t TRIG_PIN = 10;
+const uint8_t TRIGGER_PIN = 10;
 const uint8_t ECHO_PIN = 11;
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+NewPing sonar(TRIGGER_PIN, ECHO_PIN);
+volatile unsigned long latestSonarResult = 0;
 
 // IR
 const uint8_t IR0_PIN = A0;
@@ -37,13 +40,13 @@ const uint8_t IR1_PIN = A1;
 
 // For smoothing
 const uint8_t distanceFilterSize = 7;
-MedianFilter ultrasonicFilter(distanceFilterSize, 0);
+MedianFilter sonarFilter(distanceFilterSize, 0);
 MedianFilter ir0(distanceFilterSize, 0);
 MedianFilter ir1(distanceFilterSize, 0);
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   AFMS.begin();  // create with the default frequency 1.6KHz
 
@@ -52,14 +55,13 @@ void setup() {
   pinMode(IR0_PIN, INPUT);
   pinMode(IR1_PIN, INPUT);
 
+  nextMillis = millis();
   initializeSmoothing();
 
   leftMotor.setDesiredSpeed(0);
   rightMotor.setDesiredSpeed(0);
   leftMotor.drive();
   rightMotor.drive();
-
-  nextMillis = millis() + distanceSamplePeriodMillis;
 }
 
 void loop() {
@@ -75,22 +77,21 @@ void loop() {
   if (millis() > nextMillis) {
     nextMillis += distanceSamplePeriodMillis;
 
-//    leftMotor.log(Serial);
-//    rightMotor.log(Serial);
+    takeSonarReading();
+    takeIrReadings();
 
-    takeDistanceReadings();
-    uint16_t ir0Distance = ir0.out();
-    uint16_t ultrasonicDistance = ultrasonicFilter.out();
-    uint16_t ir1Distance = ir1.out();
+    uint8_t ir0Distance = ir0.out();
+    uint8_t sonarDistance = sonarFilter.out();
+    uint8_t ir1Distance = ir1.out();
 
     Serial.print(ir0Distance);
     Serial.print(' ');
-    Serial.print(ultrasonicDistance);
+    Serial.print(sonarDistance);
     Serial.print(' ');
     Serial.print(ir1Distance);
     Serial.println();
 
-    if (ultrasonicDistance < 30 || ir0Distance < 30 || ir1Distance < 30) {
+    if (sonarDistance < 30 || ir0Distance < 30 || ir1Distance < 30) {
       leftMotor.setDesiredSpeed(0);
       rightMotor.setDesiredSpeed(0);
     }
@@ -103,18 +104,42 @@ void loop() {
 void initializeSmoothing() {
   // fill buffer with readings
   for (uint8_t i = 0; i < distanceFilterSize; i++) {
-    takeDistanceReadings();
-    delay(25);
+    while (millis() < nextMillis);
+    nextMillis += distanceSamplePeriodMillis;
+    handleSonarResult(sonar.ping());
+    takeIrReadings();
   }
 }
 
-// Need to wait 38ms +/- 10ms between IR readings,
-// so you'll probably need a delay(25); after calling this
-void takeDistanceReadings() {
+void takeSonarReading() {
+  sonar.timer_stop();
+  unsigned long sonarResult = latestSonarResult; // Not worried about a data race because we stopped the timer interrupt
+  latestSonarResult = 0;
+  sonar.ping_timer(echoCheck); // Start the next one ASAP
+  handleSonarResult(sonarResult ? sonarResult : NO_ECHO);
+}
+
+void echoCheck() {
+  if (sonar.check_timer()) {
+    latestSonarResult = sonar.ping_result;
+  }
+}
+
+void handleSonarResult(unsigned long pingTimeMicroseconds) {
+  uint8_t noisyDistanceInCm =
+    (pingTimeMicroseconds == NO_ECHO ||
+    pingTimeMicroseconds > UINT_MAX) ?
+      maxDistance :
+      constrain(
+        NewPing::convert_cm(pingTimeMicroseconds),
+        minDistance,
+        maxDistance);
+  sonarFilter.in(noisyDistanceInCm);
+}
+
+void takeIrReadings() {
   ir0.in(irReadingToDistance(analogRead(IR0_PIN)));
   ir1.in(irReadingToDistance(analogRead(IR1_PIN)));
-  ultrasonicFilter.in(constrain(
-      sr04.Distance(), minDistance, maxDistance)); // includes a delay(25)
 }
 
 // Returns distance in cm
@@ -123,5 +148,4 @@ uint8_t irReadingToDistance(uint16_t value) {
   return constrain(
       (6787.0 / (value - 3.0)) - 4.0, minDistance, maxDistance);
 }
-
 
